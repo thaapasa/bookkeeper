5e2ef7d4247aaab3d7f8dd749b0205d7bbe838b0
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2016-12-20 13:22:52 +0200

    Fixed update-revision script

--- script/update-revision.sh
+++ script/update-revision.sh
@@ -4,8 +4,12 @@ pushd . >/dev/null
 cd `dirname $0`/..
 
 REV=`git rev-parse HEAD | cut -c 1-8`
-echo "Updating server revision to $REV"
+FILE=src/server/revision.js
+
+echo "Updating server revision to $REV to $FILE"
 echo
-echo "\"use strict\";\n\nmodule.exports = \"$REV\";\n" >src/server/revision.js
+echo "\"use strict\";" >$FILE
+echo "" >>$FILE
+echo "module.exports = \"$REV\";" >>$FILE
 
 popd >/dev/null
