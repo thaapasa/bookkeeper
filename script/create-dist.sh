53759be964df232a8d0580dea46b389ca8ea2ed4
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-01-15 14:54:38 +0200

    Fixes #13: Rename js + css to have git revision in the filename

--- script/create-dist.sh
+++ script/create-dist.sh
@@ -11,8 +11,8 @@ rm -rf dist
 mkdir -p dist
 cp -rf public/* dist/
 
-#mv dist/css/main.css dist/css/main-$REV.css
-#mv dist/js/calculators.min.js dist/js/calculators.min-$REV.js
-#sed -e "s/css\/main\.css/css\/main-$REV.css/g" -e "s/js\/calculators\.js/js\/calculators.min-$REV.js/g" -i dist/index.html
+mv dist/css/bookkeeper.css dist/css/bookkeeper-$REV.css
+mv dist/js/bookkeeper.js dist/js/bookkeeper-$REV.js
+sed -e "s/css\/bookkeeper\.css/css\/bookkeeper-$REV.css/g" -e "s/js\/bookkeeper\.js/js\/bookkeeper-$REV.js/g" -i -- dist/index.html
 
 popd >/dev/null
