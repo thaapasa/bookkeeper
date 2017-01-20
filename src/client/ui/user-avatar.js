79c84760552a0971d37babebd794795f75292f2d
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-01-20 18:34:44 +0200

    Converted user avatar to a function

--- src/client/ui/user-avatar.js
+++ src/client/ui/user-avatar.js
@@ -5,23 +5,16 @@ import Avatar from 'material-ui/Avatar';
 import * as state from "../data/state";
 import {cyan500, cyan900} from 'material-ui/styles/colors';
 
-export default class UserAvatar extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const user = state.get("userMap")[this.props.userId];
-        return user && user.id ?
-            <Avatar style={this.props.style}
-                    color={cyan900}
-                    size={this.props.size}
-                    backgroundColor={cyan500}
-                    className={ "user-avatar" + (this.props.className ? " " + this.props.className : "") }
-                    src={ user.image || undefined }
-                    title={ `${user.firstName} ${user.lastName}` }
-                    onClick={x => this.props.onClick && this.props.onClick(user.id)}>{ user.image ? undefined : user.firstName.charAt(0)}</Avatar>
-            : <div />
-    }
+export default function UserAvatar(props) {
+    const user = state.get("userMap")[props.userId];
+    return user && user.id ?
+        <Avatar style={props.style}
+                color={cyan900}
+                size={props.size}
+                backgroundColor={cyan500}
+                className={ "user-avatar" + (props.className ? " " + props.className : "") }
+                src={ user.image || undefined }
+                title={ `${user.firstName} ${user.lastName}` }
+                onClick={x => props.onClick && props.onClick(user.id)}>{ user.image ? undefined : user.firstName.charAt(0)}</Avatar>
+        : null
 }
