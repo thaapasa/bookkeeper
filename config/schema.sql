6d23a4b400ecc8168bc06cdc6cf2b25ec1a90b53
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-02-11 13:08:15 +0200

    Added comments on database tables and columns

--- config/schema.sql
+++ config/schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS groups (
   id SERIAL PRIMARY KEY,
   name VARCHAR(128)
 );
+COMMENT ON TABLE groups IS 'All expenses belong to a single group. Users can be part of many groups.';
 
 CREATE TABLE IF NOT EXISTS sources (
   id SERIAL PRIMARY KEY,
@@ -11,6 +12,10 @@ CREATE TABLE IF NOT EXISTS sources (
   image VARCHAR(32) DEFAULT NULL
 );
 CREATE INDEX "sources_group_id" ON sources (group_id);
+COMMENT ON TABLE sources IS 'Sources define how the costs of an expense are divided between group members';
+COMMENT ON COLUMN sources.abbreviation IS 'Abbreviation is shown in lists if image is not defined';
+COMMENT ON COLUMN sources.image IS 'Relative path to an image depicting the source';
+
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
@@ -23,6 +28,11 @@ CREATE TABLE IF NOT EXISTS users (
   image VARCHAR(32) DEFAULT NULL
 );
 CREATE INDEX "users_email" ON users (email);
+COMMENT ON TABLE users IS 'Registered users';
+COMMENT ON COLUMN users.username IS 'Username is used for logging in. Uniquely identifies the user.';
+COMMENT ON COLUMN users.password IS 'Password is used for logging in. Stored value is password hashed with SHA1.';
+COMMENT ON COLUMN users.default_group_id IS 'This group id is used when no other group id is specified.';
+COMMENT ON COLUMN users.image IS 'Avatar image of user';
 
 CREATE TABLE IF NOT EXISTS group_users (
   user_id INTEGER REFERENCES users (id) NOT NULL,
@@ -30,6 +40,8 @@ CREATE TABLE IF NOT EXISTS group_users (
   default_source_id INTEGER REFERENCES sources (id) DEFAULT NULL
 );
 CREATE INDEX "group_users_user_id" ON group_users(user_id);
+COMMENT ON TABLE group_users IS 'Links between users and groups.';
+COMMENT ON COLUMN group_users.default_source_id IS 'Default source in expenses. May be initially selected in UI.';
 
 CREATE TABLE IF NOT EXISTS source_users (
   source_id INTEGER REFERENCES sources (id) NOT NULL,
@@ -37,8 +49,6 @@ CREATE TABLE IF NOT EXISTS source_users (
   share INTEGER
 );
 CREATE INDEX "source_users_source_id_user_id" ON source_users (source_id, user_id);
-
-
 
 
 CREATE TABLE IF NOT EXISTS sessions (
@@ -49,6 +59,8 @@ CREATE TABLE IF NOT EXISTS sessions (
 );
 CREATE INDEX "sessions_token" ON sessions (token);
 CREATE INDEX "sessions_expiry" ON sessions (expiry_time);
+COMMENT ON TABLE sessions IS 'Active sessions and expiry tokens.';
+COMMENT ON COLUMN sessions.token IS 'Access token, used to authorize operations, if refresh_token is also specified. If refresh_token field is NULL, then this is the refresh token and may only be used to authorize session refresh.';
 
 
 CREATE TABLE IF NOT EXISTS categories (
@@ -58,6 +70,7 @@ CREATE TABLE IF NOT EXISTS categories (
   name VARCHAR(50) NOT NULL
 );
 CREATE INDEX "categories_group_id" ON categories (group_id);
+COMMENT ON TABLE categories IS 'Expense categories';
 
 
 
@@ -79,6 +92,7 @@ CREATE TABLE IF NOT EXISTS expenses (
   category_id INTEGER REFERENCES categories (id) NOT NULL
 );
 CREATE INDEX "expenses_group_date" ON expenses (group_id, date);
+COMMENT ON TABLE expenses IS 'Expenses';
 
 CREATE TABLE IF NOT EXISTS expense_division (
   expense_id INTEGER REFERENCES expenses (id) ON DELETE CASCADE NOT NULL,
@@ -86,3 +100,4 @@ CREATE TABLE IF NOT EXISTS expense_division (
   type expense_type NOT NULL,
   sum MONEY NOT NULL
 );
+COMMENT ON TABLE expense_division IS 'Describes how each expense item is divided between group users';
