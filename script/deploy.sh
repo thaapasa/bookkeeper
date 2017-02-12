c9a56546ebc5d1c7764a975698f24ba0cb831d5b
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-02-12 11:42:18 +0200

    Fixed deploy script to login before running update (uses correct locale)

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh deployer@pomeranssi.fi "cd ~/bookkeeper && git pull && script/build-prod.sh"
+ssh deployer@pomeranssi.fi "bash --login -c 'cd ~/bookkeeper && git pull && script/build-prod.sh'"
