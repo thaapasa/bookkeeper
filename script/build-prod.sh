05c3a73417cdcb7cd3d7e2398ab39331f618a16e
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-02-12 11:31:38 +0200

    Made update script backup production database before updating

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Deploying bookkeeper to production..."
+
 pushd . >/dev/null
 cd `dirname $0`/..
 
@@ -7,9 +9,13 @@ npm install
 
 script/stop-server.sh
 
+~/bin/backup-bookkeeper.sh before-prod-update
+
 npm run prod && \
     echo "Build successful!"
 
 script/start-server.sh
 
 popd >/dev/null
+
+echo "Deployment done."
