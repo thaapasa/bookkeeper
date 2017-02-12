55b45789eb7ff42a7817aa05d422dc23dc207e31
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-02-12 11:40:53 +0200

    Added git revision number to production backup identifier

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-echo "Deploying bookkeeper to production..."
 
 pushd . >/dev/null
 cd `dirname $0`/..
+
+REV=`git rev-parse HEAD | cut -c 1-8`
+echo "Deploying bookkeeper to production, revision $REV..."
 
 npm install
 
 script/stop-server.sh
 
-~/bin/backup-bookkeeper.sh before-prod-update
+~/bin/backup-bookkeeper.sh "before-update-$REV"
 
 npm run prod && \
     echo "Build successful!"
