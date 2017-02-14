1e7ccdfaee292ee627cc3ff1ece2716029a7e5c0
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-02-14 17:47:57 +0200

    Fixed #42: Expenses can be navigated with the arrow keys

--- src/client/ui/expense-navigation.js
+++ src/client/ui/expense-navigation.js
@@ -4,23 +4,48 @@ import * as time from "../../shared/util/time"
 import * as colors from "./colors";
 import {NavigateLeft,NavigateRight} from "./icons";
 import * as state from "../data/state";
+import {KeyCodes} from "../util/io";
 const Moment = require("moment");
 
 export default class ExpenseNavigation extends React.Component {
 
     constructor(props) {
         super(props);
+        this.navigateMonths = this.navigateMonths.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     static getYearMonthString(date) {
         return time.getFinnishMonthName(date.month() + 1) + " " + date.year();
     }
 
+    navigateMonths(offset) {
+        state.updateExpenses(this.props.date.clone().add(offset, 'months'));
+    }
+
+    componentDidMount() {
+        this.container.focus();
+    }
+
+    handleKeyPress(event) {
+        switch (event.keyCode) {
+            case KeyCodes.right:
+                this.navigateMonths(1);
+                return false;
+            case KeyCodes.left:
+                this.navigateMonths(-1);
+                return false;
+        }
+    }
+
     render() {
-        return <div style={{ display: "flex", alignItems: "center" }}>
+        return <div style={{ display: "flex", alignItems: "center" }}
+                    onKeyUp={this.handleKeyPress}
+                    tabIndex={1}
+                    ref={r => this.container = r}>
             <div>
                 <IconButton
-                    onClick={() => state.updateExpenses(this.props.date.clone().subtract(1, 'months')) }
+                    onClick={() => this.navigateMonths(-1)}
                     title="Edellinen"
                     style={{ padding: "0px" }}><NavigateLeft color={colors.navigation} /></IconButton>
             </div>
@@ -29,7 +54,7 @@ export default class ExpenseNavigation extends React.Component {
             </div>
             <div>
                 <IconButton
-                    onClick={() => state.updateExpenses(this.props.date.clone().add(1, 'months')) }
+                    onClick={() => this.navigateMonths(1)}
                     title="Seuraava"><NavigateRight color={colors.navigation} /></IconButton>
             </div>
         </div>
--- src/client/util/io.js
+++ src/client/util/io.js
@@ -2,5 +2,13 @@
 
 export const KeyCodes = {
     enter: 13,
-    escape: 27
+    escape: 27,
+    left: 37,
+    right: 39,
+    up: 38,
+    down: 40,
+    shift: 16,
+    alt: 18,
+    ctr: 17,
+    backspace: 8
 };
