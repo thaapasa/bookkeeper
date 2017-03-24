1d9c4b1a2650f668c7f369d385608d89c4fd75f9
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-03-24 18:03:38 +0200

    Fixed #51: Added react-router-dom. Added navigation between expenses & categories.

---
+++ src/client/ui/category-view.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default class CategoryView extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="content">
+            Kategoriat
+        </div>
+    }
+}
--- src/client/ui/page.js
+++ src/client/ui/page.js
@@ -1,10 +1,13 @@
 import React from "react"
 import TopBar from "./topbar"
 import MonthView from "./month-view"
+import CategoryView from "./category-view"
 import ExpenseDialog from "./expense-dialog"
 import ConfirmationDialog from "./confirmation-dialog"
 import NotificationBar from "./notification-bar"
 import DatePickerComponent from "./date-picker-component"
+import FlatButton from "material-ui/FlatButton";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import * as log from "../../shared/util/log"
 
 export default class BookkeeperPage extends React.Component {
@@ -27,10 +30,19 @@ export default class BookkeeperPage extends React.Component {
         return <div className="everything">
             <ExpenseDialog />
             <ConfirmationDialog />
-            <TopBar user={this.state.user} />
-            <div className="main-content">
-                <div><MonthView/></div>
-            </div>
+            <Router>
+                <div>
+                    <TopBar user={this.state.user}>
+                        <Link to="/kulut"><FlatButton primary={true}>Kulut</FlatButton></Link>
+                        <Link to="/kategoriat"><FlatButton>Kategoriat</FlatButton></Link>
+                    </TopBar>
+                    <div className="main-content">
+                        <Route exact path="/" component={MonthView} />
+                        <Route path="/kulut" component={MonthView} />
+                        <Route path="/kategoriat" component={CategoryView} />
+                    </div>
+                </div>
+            </Router>
             <DatePickerComponent />
             <NotificationBar />
         </div>
