4ecf607cda2e6a2ef55795ce405f3a71ae838b0e
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-12-28 12:15:51 +0200

    Add client index file (rename from client.js)

---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+/*
+ * This is Bookkeeper client (web app) index file 
+ */
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import registerServiceWorker from './client/registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import BookkeeperPage from './client/ui/page';
+import LoginPage from './client/ui/login-page';
+import './index.css';
+import * as log from "./shared/util/log"
+import * as login from "./client/data/login"
+
+(window as any).log = log;
+
+injectTapEventPlugin();
+
+interface BookkeeperState {
+    session: string | undefined;
+    initialized: boolean;
+}
+
+class Bookkeeper extends React.Component<{}, BookkeeperState> {
+
+    public state: BookkeeperState = {
+        session: undefined,
+        initialized: false,
+    };
+
+    public async componentDidMount() {
+        log.info("Initializing bookkeeper client");
+        login.currentSession.onValue((u: string) => this.setState({ session: u }));
+        
+        await login.checkLoginState();
+        this.setState({ initialized: true });
+    }
+  
+    public render() {
+        return (this.state.initialized) ?
+            ((this.state.session === undefined) ? <LoginPage /> : <BookkeeperPage session={ this.state.session } />) :
+            <div />;
+    }
+}
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <Bookkeeper />
+    </MuiThemeProvider>, 
+    document.getElementById("root") as HTMLElement
+);
+registerServiceWorker();
