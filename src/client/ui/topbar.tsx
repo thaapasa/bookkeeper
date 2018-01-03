84d65720dbeab6c7f37c617401317bd39000f8bd
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-01-03 14:27:48 +0200

    Add types to topbar

--- src/client/ui/topbar.tsx
+++ src/client/ui/topbar.tsx
@@ -1,34 +1,34 @@
 import * as React from 'react';
-import UserAvatar from "./user-avatar";
+import UserAvatar from './user-avatar';
 import RaisedButton from 'material-ui/RaisedButton';
-import IconButton from "material-ui/IconButton";
+import IconButton from 'material-ui/IconButton';
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
-import * as state from "../data/state";
-import * as colors from "./colors";
-import { logout } from "../data/login"
+import * as state from '../data/state';
+import * as colors from './colors';
+import { logout } from '../data/login'
 
-const buttonStyle = { float : "right" };
+const buttonStyle = { float : 'right' };
 
-export default class ToolbarExamplesSimple extends React.Component {
+interface TopBarProps {
+    user: { id: any };
+}
 
-    constructor(props) {
-        super(props);
-    }
+export default class TopBar extends React.Component<TopBarProps, {}> {
 
-    handleClick() {
+    private handleClick = () => {
         state.editExpense(undefined);
     }
 
-    render() {
+    public render() {
         return (
             <Toolbar className="top-bar">
                 <ToolbarGroup className="optional">
-                    <ToolbarTitle text={ state.getTitle() } />
+                    <ToolbarTitle text={state.getTitle()} />
                 </ToolbarGroup>
                 <ToolbarGroup>
                     { this.props.children }
                 </ToolbarGroup>
-                <ToolbarGroup style={{ align: "right" }}>
+                <ToolbarGroup style={{ align: 'right' }}>
                     <RaisedButton label="Kirjaa" primary={true} style={buttonStyle} onTouchTap={this.handleClick} />
                     <UserAvatar userId={this.props.user.id} />
                     <IconButton iconClassName="material-icons" iconStyle={{ color: colors.tool }} onClick={logout}>exit_to_app</IconButton>
