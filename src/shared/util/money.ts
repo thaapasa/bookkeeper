20d92c84e4ea4d439d4ee0446f74850bbfa2778d
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-01-12 17:05:23 +0200

    Make money class better

--- src/shared/util/money.ts
+++ src/shared/util/money.ts
@@ -4,21 +4,23 @@ Big.DP = 2;
 // Round down (truncate)
 Big.RM = 0;
 
+type MoneyInputType = number | Big | Money | string;
+
 export default class Money {
 
     public value: Big;
 
-    static toBig(m: any): Big {
+    public static toBig(m: MoneyInputType): Big {
         if (m instanceof Big) return m;
         if (m instanceof Money) return m.value;
         return Big(m);
     }
 
-    constructor(value: any) {
+    public constructor(value: MoneyInputType) {
         this.value = Money.toBig(value);
     }
 
-    static from(value: any, defaultValue?: any): Money {
+    public static from(value: MoneyInputType | null, defaultValue?: MoneyInputType): Money {
         if (value === null || value === undefined) {
             if (defaultValue === undefined) { throw new Error('Money.from(undefined)'); }
             return Money.from(defaultValue);
@@ -27,58 +29,58 @@ export default class Money {
         return new Money(value);
     }
 
-    static orZero(value: any): Money {
+    public static orZero(value: MoneyInputType | null | undefined): Money {
         return value ? Money.from(value) || Money.zero : Money.zero;
     }
 
-    toString(scale?: number): string {
+    public toString(scale?: number): string {
         if (scale === undefined) scale = 2;
         return this.value.toFixed(scale);
     }
 
-    format(scale?: number): string {
+    public format(scale?: number): string {
         if (scale === undefined) scale = 2;
         return `${this.value.toFixed(scale)} €`;
     }
 
-    inspect(): string {
+    public inspect(): string {
         return this.format();
     }
 
-    plus(o: any): Money {
+    public plus(o: any): Money {
         return new Money(this.value.plus(Money.toBig(o)));
     }
 
-    minus(o: any): Money {
+    public minus(o: any): Money {
         return new Money(this.value.minus(Money.toBig(o)));
     }
 
-    toCents(): number {
+    public toCents(): number {
         return parseInt(this.value.times(100).toFixed(0));
     }
 
-    gte(o: any): boolean { return this.value.gte(Money.toBig(o)); }
-    gt(o: any): boolean { return this.value.gt(Money.toBig(o)); }
-    lte(o: any): boolean { return this.value.lte(Money.toBig(o)); }
-    lt(o: any): boolean { return this.value.lt(Money.toBig(o)); }
+    public gte(o: MoneyInputType): boolean { return this.value.gte(Money.toBig(o)); }
+    public gt(o: MoneyInputType): boolean { return this.value.gt(Money.toBig(o)); }
+    public lte(o: MoneyInputType): boolean { return this.value.lte(Money.toBig(o)); }
+    public lt(o: MoneyInputType): boolean { return this.value.lt(Money.toBig(o)); }
 
-    divide(o: any): Money {
+    public divide(o: MoneyInputType): Money {
         return new Money(this.value.div(Money.toBig(o)));
     }
 
-    multiply(o: any): Money {
+    public multiply(o: MoneyInputType): Money {
         return new Money(this.value.times(Money.toBig(o)));
     }
 
-    negate(): Money {
+    public negate(): Money {
         return new Money(this.value.times(-1));
     }
 
-    equals(o: any): boolean {
+    public equals(o: MoneyInputType): boolean {
         return this.value.eq(Money.toBig(o));
     }
 
-    static zero = new Money("0");
-    static euro = new Money("1");
-    static cent = new Money("0.01");
+    public static zero = new Money('0');
+    public static euro = new Money('1');
+    public static cent = new Money('0.01');
 }
