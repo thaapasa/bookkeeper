3991a6b5c8e902b9d954403d79a4b72482f0073c
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-01-13 14:09:24 +0200

    Install styled-components and use it in login-page

--- src/client/ui/login-page.tsx
+++ src/client/ui/login-page.tsx
@@ -2,100 +2,110 @@ import * as React from 'react';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import * as login from "../data/login"
+import * as login from '../data/login';
+import * as apiConnect from '../data/api-connect';
+import styled from 'styled-components';
+import { CSSProperties } from 'react';
 const debug = require('debug')('bookkeeper:login-page');
 
-const apiConnect = require("../data/api-connect");
-
-const paperStyle = {
-    margin: "20px",
-    padding: "30px",
+const paperStyle: CSSProperties = {
+    margin: '20px',
+    padding: '30px',
     textAlign: 'center',
-    display: 'inline-block'
+    display: 'inline-block',
 };
 
-const titleStyle = {
-    height: "30px",
-    display: "inline-block",
-    maxWidth: "250px"
+const titleStyle: CSSProperties = {
+    height: '30px',
+    display: 'inline-block',
+    maxWidth: '250px',
 };
 
-const loginButtonStyle = {
-    margin: "30px"
+const loginButtonStyle: CSSProperties = {
+    margin: '30px',
 };
 
-export default class LoginPage extends React.Component<any, any> {
+const Page = styled.div`
+    margin: auto;
+    text-align: center;
+`;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            showStatusMessage: false,
-            statusMessage: "plaa"
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleLoginError = this.handleLoginError.bind(this);
-    }
+interface LoginPageState {
+    username: string;
+    password: string;
+    showStatusMessage: boolean;
+    statusMessage: string;
+}
 
-    handleLoginError(er) {
+export default class LoginPage extends React.Component<{}, LoginPageState> {
+
+    public state: LoginPageState = {
+        username: '',
+        password: '',
+        showStatusMessage: false,
+        statusMessage: '',
+    };
+
+    private handleLoginError = (er) => {
         if (er && er.status === 401) {
             this.setState({
-                statusMessage: "Kirjautuminen epäonnistui. Ole hyvä ja tarkista käyttäjätunnuksesi ja salasanasi.",
-                showStatusMessage : true
-            })
+                statusMessage: 'Kirjautuminen epäonnistui. Ole hyvä ja tarkista käyttäjätunnuksesi ja salasanasi.',
+                showStatusMessage: true,
+            });
         } else {
             this.setState({
-                statusMessage: "Kirjautumisessa tapahtui virhe.",
-                showStatusMessage : true
-            })
+                statusMessage: 'Kirjautumisessa tapahtui virhe.',
+                showStatusMessage: true,
+            });
         }
     }
 
-    handleSubmit(event) {
+    private handleSubmit = async (event) => {
         event.preventDefault();
         this.setState({
-            statusMessage: "",
-            showStatusMessage : false
-        })
-        apiConnect.login(this.state.username, this.state.password)
-            .then(u => {
-                debug("logged in", u);
-                login.loginStream.push(u);
-                return null;
-            })
-            .catch(this.handleLoginError);
+            statusMessage: '',
+            showStatusMessage: false,
+        });
+        try {
+            const user = await apiConnect.login(this.state.username, this.state.password);
+            login.loginStream.push(user);
+            return null;
+        } catch (e) {
+            this.handleLoginError(e);
+        }
     }
 
     public render() {
-        return <div className="everything">
-            <Paper style={paperStyle} zDepth={1}>
-                <form onSubmit={this.handleSubmit}>
-                    <title style={titleStyle} >Kirjaudu sisään</title>
-                    <br/>
-                    <TextField
-                        hintText="Käyttäjätunnus"
-                        floatingLabelText="Käyttäjätunnus"
-                        value={this.state.username}
-                        onChange={(e, v) => this.setState({ username: v })}
-                    /><br />
-                    <TextField
-                        hintText="Salasana"
-                        floatingLabelText="Salasana"
-                        type="password"
-                        value={this.state.password}
-                        onChange={(e, v) => this.setState({ password: v })}
-                    /><br />
-                    <RaisedButton
-                        type="submit"
-                        label="Kirjaudu"
-                        primary={true}
-                        style={loginButtonStyle}
+        return (
+            <Page>
+                <Paper style={paperStyle} zDepth={1}>
+                    <form onSubmit={this.handleSubmit}>
+                        <title style={titleStyle}>Kirjaudu sisään</title>
+                        <br/>
+                        <TextField
+                            hintText="Käyttäjätunnus"
+                            floatingLabelText="Käyttäjätunnus"
+                            value={this.state.username}
+                            onChange={(e, v) => this.setState({ username: v })}
                         /><br />
-                    {this.state.showStatusMessage ? <title style={titleStyle}>{this.state.statusMessage}</title> : ""}
-                </form>
-            </Paper>
-        </div>;
+                        <TextField
+                            hintText="Salasana"
+                            floatingLabelText="Salasana"
+                            type="password"
+                            value={this.state.password}
+                            onChange={(e, v) => this.setState({ password: v })}
+                        /><br />
+                        <RaisedButton
+                            type="submit"
+                            label="Kirjaudu"
+                            primary={true}
+                            style={loginButtonStyle}
+                            /><br />
+                        {this.state.showStatusMessage ? <title style={titleStyle}>{this.state.statusMessage}</title> : ""}
+                    </form>
+                </Paper>
+            </Page>
+        );
     }
 
 }
