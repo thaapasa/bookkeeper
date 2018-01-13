4d4042ac57f04f0c4cb8d4d33e937bfb7220ec5a
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-01-13 14:16:16 +0200

    Clean up page code

--- src/client/ui/page.tsx
+++ src/client/ui/page.tsx
@@ -1,70 +1,51 @@
 import * as React from 'react';
-import TopBar from "./topbar"
-import MonthView from "./month-view"
-import CategoryView from "./category-view"
-import ExpenseDialog from "./expense-dialog"
-import ConfirmationDialog from "./confirmation-dialog"
-import NotificationBar from "./notification-bar"
-import DatePickerComponent from "./date-picker-component"
-import FlatButton from "material-ui/FlatButton";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import TopBar from './topbar';
+import MonthView from './month-view';
+import CategoryView from './category-view';
+import ExpenseDialog from './expense-dialog';
+import ConfirmationDialog from './confirmation-dialog';
+import NotificationBar from './notification-bar';
+import DatePickerComponent from './date-picker-component';
+import FlatButton from 'material-ui/FlatButton';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const debug = require('debug')('bookkeeper:page');
 
-const TopBarJs: any = TopBar;
-
-const LinkButton = ({ label, to }) => (
-    <Route path={to} exact={true} children={ ({ match }) => <Link to={to}><FlatButton primary={!!match}>{label}</FlatButton></Link> }/>
-)
+function LinkButton({ label, to }: { label: string, to: string }) {
+    return (
+        <Route path={to} exact={true} children={({ match }) => 
+            <Link to={to}>
+                <FlatButton primary={!!match}>{label}</FlatButton>
+            </Link>} />
+    );
+}
 
 interface PageProps {
     session: { [key: string]: any };
 }
-interface PageState {
-    session: any;
-    user: any;
-    categories: any;
-    groups: any;
-    users: any;
-    sources: any;
-}
 
-export default class BookkeeperPage extends React.Component<PageProps, PageState> {
-
-    public state: PageState;
-
-    constructor(props: PageProps) {
-        super(props);
-        debug("Initializing bookkeeper page");
-        const session = this.props.session;
-        this.state = {
-            session: session,
-            user: session.user,
-            categories: session.categories,
-            groups: session.groups,
-            users: session.users,
-            sources: session.sources
-        };
-    }
+export default class BookkeeperPage extends React.Component<PageProps, {}> {
 
     public render() {
-        return <div className="everything">
-            <ExpenseDialog />
-            <ConfirmationDialog />
-            <Router>
-                <div>
-                    <TopBarJs user={this.state.user}>
-                        <LinkButton label="Kulut" to="/" />
-                        <LinkButton label="Kategoriat" to="/p/kategoriat" />
-                    </TopBarJs>
-                    <div className="main-content">
-                        <Route exact path="/" component={MonthView} />
-                        <Route path="/p/kulut" component={MonthView} />
-                        <Route path="/p/kategoriat" component={CategoryView} />
+        return (
+            <div>
+                <ExpenseDialog />
+                <ConfirmationDialog />
+                <Router>
+                    <div>
+                        <TopBar user={this.props.session.user}>
+                            <LinkButton label="Kulut" to="/" />
+                            <LinkButton label="Kategoriat" to="/p/kategoriat" />
+                        </TopBar>
+                        <div className="main-content">
+                            <Route exact path="/" component={MonthView} />
+                            <Route path="/p/kulut" component={MonthView} />
+                            <Route path="/p/kategoriat" component={CategoryView} />
+                        </div>
                     </div>
-                </div>
-            </Router>
-            <DatePickerComponent />
-            <NotificationBar />
-        </div>
+                </Router>
+                <DatePickerComponent />
+                <NotificationBar />
+            </div>
+        );
     }
 }
