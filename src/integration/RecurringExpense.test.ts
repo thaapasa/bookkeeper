16d6c6504ea749aa7d38faa6f09467f4058b2899
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-01-31 20:25:21 +0200

    Format test files with 2 space indentation

--- src/integration/RecurringExpense.test.ts
+++ src/integration/RecurringExpense.test.ts
@@ -8,49 +8,50 @@ import { ApiMessage } from '../shared/types/Api';
 
 describe('recurringExpense', () => {
 
-    let session: SessionWithControl;
+  let session: SessionWithControl;
 
-    const newExpense = help.newExpense;
+  const newExpense = help.newExpense;
 
-    beforeEach(async () => { session = await getSession('sale', 'salasana'); });
-    afterEach(async () => { await help.cleanup(session); });
+  beforeEach(async () => { session = await getSession('sale', 'salasana'); });
+  afterEach(async () => { await help.cleanup(session); });
 
-    async function checkMonthStatus(expectedBenefit?: MoneyLike, expectItems?: (items: UserExpense[]) => any): Promise<Money> {
-        const m = await session.get<ExpenseCollection>(`/api/expense/month`, { year: 2017, month: 1 });
-        expect(m).toHaveProperty('monthStatus');
-        expect(m.monthStatus).toHaveProperty('benefit');
-        if (expectedBenefit) {
-            expect(m.monthStatus.benefit).toEqual(expectedBenefit);
-        }
-        expect(m).toHaveProperty('expenses');
-        expect(m.expenses).toBeInstanceOf(Array);
-        if (expectItems) {
-            expectItems(m.expenses);
-        }
-        return Money.from(m.monthStatus.benefit);
+  async function checkMonthStatus(expectedBenefit?: MoneyLike, expectItems?: (items: UserExpense[]) => any): Promise<Money> {
+    const m = await session.get<ExpenseCollection>(`/api/expense/month`, { year: 2017, month: 1 });
+    expect(m).toHaveProperty('monthStatus');
+    expect(m.monthStatus).toHaveProperty('benefit');
+    if (expectedBenefit) {
+      expect(m.monthStatus.benefit).toEqual(expectedBenefit);
     }
+    expect(m).toHaveProperty('expenses');
+    expect(m.expenses).toBeInstanceOf(Array);
+    if (expectItems) {
+      expectItems(m.expenses);
+    }
+    return Money.from(m.monthStatus.benefit);
+  }
 
-    it('recurring expense template should not show up on expense queries', async () => {
-        const monthBenefit1 = await checkMonthStatus();
-        const expenseId = checkCreateStatus(await newExpense(session, {
-            sum: '150.00', confirmed: false, date: '2017-01-15', title: 'Tonnikalaa' }));
+  it('recurring expense template should not show up on expense queries', async () => {
+    const monthBenefit1 = await checkMonthStatus();
+    const expenseId = checkCreateStatus(await newExpense(session, {
+      sum: '150.00', confirmed: false, date: '2017-01-15', title: 'Tonnikalaa'
+    }));
 
-        const e = await session.get<Expense>(`/api/expense/${expenseId}`);
-        expect(e).toHaveProperty('division');
-        expect(e.division).toContainEqual({ userId: 2, type: 'benefit', sum: '75.00' });
-        expect(e).toHaveProperty('recurringExpenseId');
-        expect(e.recurringExpenseId).toBeNull;
+    const e = await session.get<Expense>(`/api/expense/${expenseId}`);
+    expect(e).toHaveProperty('division');
+    expect(e.division).toContainEqual({ userId: 2, type: 'benefit', sum: '75.00' });
+    expect(e).toHaveProperty('recurringExpenseId');
+    expect(e.recurringExpenseId).toBeNull;
 
-        const monthBenefit2 = await checkMonthStatus(monthBenefit1.plus('75').toString(),
-                e => expect(e.find(i => i.id === expenseId)).toBeTruthy);
-        const s = await session.put<ApiMessage>(`/api/expense/recurring/${expenseId}`, { period: 'monthly' });
-        expect(s.recurringExpenseId).toBeGreaterThan(0);
-        expect(s.templateExpenseId).toBeGreaterThan(0);
-        help.captureId(s.templateExpenseId);
-        checkMonthStatus(monthBenefit2.toString());
+    const monthBenefit2 = await checkMonthStatus(monthBenefit1.plus('75').toString(),
+      e => expect(e.find(i => i.id === expenseId)).toBeTruthy);
+    const s = await session.put<ApiMessage>(`/api/expense/recurring/${expenseId}`, { period: 'monthly' });
+    expect(s.recurringExpenseId).toBeGreaterThan(0);
+    expect(s.templateExpenseId).toBeGreaterThan(0);
+    help.captureId(s.templateExpenseId);
+    checkMonthStatus(monthBenefit2.toString());
 
-        const e2 = await session.get<Expense>(`/api/expense/${expenseId}`);
-        expect(e2.recurringExpenseId).toEqual(s.recurringExpenseId);
-    });
+    const e2 = await session.get<Expense>(`/api/expense/${expenseId}`);
+    expect(e2.recurringExpenseId).toEqual(s.recurringExpenseId);
+  });
 
 });
--- src/integration/Session.test.ts
+++ src/integration/Session.test.ts
@@ -5,71 +5,71 @@ import { SessionWithControl } from '../shared/util/test/TestClient';
 import { expectThrow } from '../shared/util/test/TestUtil';
 
 function checkSession(s: Session) {
-    expect(s.users).toBeInstanceOf(Array);
-    expect(s.groups).toBeInstanceOf(Array);
-    expect(typeof s.token).toBe('string');
-    expect(s.token.length).toEqual(40);
-    expect(typeof s.refreshToken).toBe('string');
-    expect(s.refreshToken.length).toEqual(40);
-    expect(s.sources).toBeInstanceOf(Array);
-    expect(s.categories).toBeInstanceOf(Array);
-    expect(typeof s.user).toBe('object');
-    expect(typeof s.group).toBe('object');
-    expect(s.user).toMatchObject({ firstName: 'Sauli', lastName: 'Niinistö' });
-    expect(s.group).toMatchObject({ name: 'Mäntyniemi' });
-    return s;
+  expect(s.users).toBeInstanceOf(Array);
+  expect(s.groups).toBeInstanceOf(Array);
+  expect(typeof s.token).toBe('string');
+  expect(s.token.length).toEqual(40);
+  expect(typeof s.refreshToken).toBe('string');
+  expect(s.refreshToken.length).toEqual(40);
+  expect(s.sources).toBeInstanceOf(Array);
+  expect(s.categories).toBeInstanceOf(Array);
+  expect(typeof s.user).toBe('object');
+  expect(typeof s.group).toBe('object');
+  expect(s.user).toMatchObject({ firstName: 'Sauli', lastName: 'Niinistö' });
+  expect(s.group).toMatchObject({ name: 'Mäntyniemi' });
+  return s;
 }
 
 describe('session', () => {
 
-    function login(): Promise<SessionWithControl> {
-        return client.getSession('sale', 'salasana');
-    }
+  function login(): Promise<SessionWithControl> {
+    return client.getSession('sale', 'salasana');
+  }
 
-    const testUrl = '/api/source/list';
+  const testUrl = '/api/source/list';
 
-    it('should give session info with GET', async () => {
-        const session = await login();
-        checkSession(session);
-        const s = await session.get<Session>('/api/session');
-        checkSession(s);
-        const d = await session.get(testUrl);
-        expect(d).toBeTruthy();
-        await session.logout();
-    });
+  it('should give session info with GET', async () => {
+    const session = await login();
+    checkSession(session);
+    const s = await session.get<Session>('/api/session');
+    checkSession(s);
+    const d = await session.get(testUrl);
+    expect(d).toBeTruthy();
+    await session.logout();
+  });
 
-    it('should not return data without token', async () => {
-        await expectThrow(() => client.get('', testUrl));
-    });
+  it('should not return data without token', async () => {
+    await expectThrow(() => client.get('', testUrl));
+  });
 
-    it('should not allow GET after logout', async () => {
-        const session = await login();
-        await session.logout();
-        await expectThrow(() => session.get(testUrl));
-    });
+  it('should not allow GET after logout', async () => {
+    const session = await login();
+    await session.logout();
+    await expectThrow(() => session.get(testUrl));
+  });
 
-    it('should not allow API access with refreshToken', async () => {
-        const session = await login();
-        await session.logout();
-        await expectThrow(() => client.get(session.refreshToken, testUrl));
-    });
+  it('should not allow API access with refreshToken', async () => {
+    const session = await login();
+    await session.logout();
+    await expectThrow(() => client.get(session.refreshToken, testUrl));
+  });
 
-    it('should allow refresh with refreshToken', async () => {
-        const session = await login();
-        const s = await client.refreshSession(session.refreshToken);
-        checkSession(s);
-        expect(s.token).not.toEqual(session.token);
-        expect(s.refreshToken).not.toEqual(session.refreshToken);
-        const d = await s.get(testUrl);
-        expect(d).toBeTruthy();
-        await s.logout();
-    });
+  it('should allow refresh with refreshToken', async () => {
+    const session = await login();
+    const s = await client.refreshSession(session.refreshToken);
+    checkSession(s);
+    expect(s.token).not.toEqual(session.token);
+    expect(s.refreshToken).not.toEqual(session.refreshToken);
+    const d = await s.get(testUrl);
+    expect(d).toBeTruthy();
+    await s.logout();
+  });
 
-    it('should not allow refresh with refreshToken after logout', async () => {
-        const session = await login();
-        await session.logout();
-        await expectThrow(() => session.get(testUrl));
-        await expectThrow(() => client.refreshSession(session.refreshToken));
-    });
+  it('should not allow refresh with refreshToken after logout', async () => {
+    const session = await login();
+    await session.logout();
+    await expectThrow(() => session.get(testUrl));
+    await expectThrow(() => client.refreshSession(session.refreshToken));
+  });
 
 });
