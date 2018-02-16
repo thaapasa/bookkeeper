9c8a3edec3b90e3dbbbd5637b0c7392a81f2e66e
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-02-16 19:45:03 +0200

    Use proper quotation marks in generated revision.ts

--- script/update-revision.sh
+++ script/update-revision.sh
@@ -7,6 +7,6 @@ REV=`git rev-parse HEAD`
 FILE=src/server/revision.ts
 
 echo "Updating server revision to $REV to $FILE"
-echo "export default \"$REV\";" >$FILE
+echo "export default '$REV';" >$FILE
 
 popd >/dev/null
