6f040b67630019afe89cdc9c12cb0dac8c734d55
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-02-24 18:55:46 +0200

    Make stop-server script not die if server is not alive

--- script/stop-server.sh
+++ script/stop-server.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 echo "Shutting down server"
-ps -ef | grep BookkeeperServer | grep -v grep | awk '{print $2;}' | xargs kill
+PIDS=`ps -ef | grep BookkeeperServer | grep -v grep | awk '{print $2;}'`
+
+if [ "$PIDS" != "" ]; then
+  echo "Killing processes $PID"
+  ps -ef | grep BookkeeperServer | grep -v grep | awk '{print $2;}' | xargs kill
+else
+  echo "Server not active"
+fi
