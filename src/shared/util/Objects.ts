3b4fed83f2ebc8289dbf70f68bef99b7a94f9e87
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2018-07-07 12:31:16 +0300

    Use new TS syntax for Omit

--- src/shared/util/Objects.ts
+++ src/shared/util/Objects.ts
@@ -8,8 +8,7 @@ export interface NumberMap<T> {
   [key: number]: T;
 }
 
-export type Diff<T extends string, U extends string> = ({[P in T]: P} & {[P in U]: never} & {[x: string]: never})[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export function pick<T, K extends keyof T>(names: ReadonlyArray<K>, obj: T): Pick<T, K> {
   const res: Pick<T, K> = {} as any;
