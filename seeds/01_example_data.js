774cb0a888d96e476996a722b8a1e165136dbf7d
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-10-12 16:44:55 +0300

    Fix server tsconfig

--- seeds/01_example_data.js
+++ seeds/01_example_data.js
@@ -5,18 +5,25 @@ exports.seed = knex => knex.raw(`
   INSERT INTO groups (name) VALUES ('Herrakerho');
 
   INSERT INTO users (username, email, password, first_name, last_name, default_group_id, image)
-  VALUES ('jenni', 'jenni@fi.fi', encode(digest('salasana', 'sha1'), 'hex'), 'Jenni', 'Haukio', 1, '1.png');
+    VALUES ('jenni', 'jenni@fi.fi', encode(digest('salasana', 'sha1'), 'hex'), 'Jenni', 'Haukio', 1, '1.png');
   INSERT INTO users (username, email, password, first_name, last_name, default_group_id, image)
-  VALUES ('sale', 'sauli@fi.fi', encode(digest('salasana', 'sha1'), 'hex'), 'Sauli', 'Niinistö', 1, '2.jpg');
+    VALUES ('sale', 'sauli@fi.fi', encode(digest('salasana', 'sha1'), 'hex'), 'Sauli', 'Niinistö', 1, '2.jpg');
 
-  INSERT INTO sources (group_id, name, image) VALUES (1, 'Yhteinen tili', 'spankki.jpg'), (1, 'Jennin tili', 'op-white.jpg'), (1, 'Salen tili', 'op-orange.jpg') RETURNING id;
-  INSERT INTO source_users (source_id, user_id, share) VALUES (1, 1, 1), (1, 2, 1), (2, 1, 1), (3, 2, 1);
+  INSERT INTO sources (group_id, name, image) VALUES 
+    (1, 'Yhteinen tili', 'spankki.png'),
+    (1, 'Jennin tili', 'op-white.png'),
+    (1, 'Salen tili', 'op-orange.png') RETURNING id;
+  INSERT INTO source_users (source_id, user_id, share) VALUES
+    (1, 1, 1), (1, 2, 1), (2, 1, 1), (3, 2, 1);
 
   INSERT INTO group_users (user_id, group_id, default_source_id) VALUES (1, 1, 1);
   INSERT INTO group_users (user_id, group_id, default_source_id) VALUES (2, 1, 1);
   INSERT INTO group_users (user_id, group_id, default_source_id) VALUES (2, 2, 2);
 
-  INSERT INTO CATEGORIES (group_id, name) VALUES (1, 'Ruoka'), (1, 'Viihde'), (1, 'Asuminen') RETURNING id;
+  INSERT INTO CATEGORIES (group_id, name) VALUES
+    (1, 'Ruoka'), (1, 'Viihde'), (1, 'Asuminen')
+    RETURNING id;
+
   INSERT INTO CATEGORIES (parent_id, group_id, name) VALUES
     (1, 1, 'Työpaikkalounas'), (1, 1, 'Ravintola'), (1, 1, 'Ruokakauppa'),
     (2, 1, 'Lehtitilaukset'), (2, 1, 'Elokuvat ja sarjat'), (2, 1, 'Kirjat'),
