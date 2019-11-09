08cf3e06755730cd8cab4ba7da2a563f586a1c54
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-11-09 20:53:40 +0200

    Add year headers to search results page

--- src/client/ui/search/ResultsView.tsx
+++ src/client/ui/search/ResultsView.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import * as B from 'baconjs';
 import styled from 'styled-components';
 import { UserExpense } from 'shared/types/Expense';
-import { secondaryColors } from '../Colors';
+import { secondaryColors, gray } from '../Colors';
 import ExpenseRow from '../expense/row/ExpenseRow';
 import { noop } from '../../../shared/util/Util';
 import { connect } from '../component/BaconConnect';
 import { userDataE, UserDataProps } from 'client/data/Categories';
-import { ExpenseTableLayout } from '../expense/row/ExpenseTableLayout';
+import {
+  ExpenseTableLayout,
+  AllColumns,
+} from '../expense/row/ExpenseTableLayout';
+import { toMoment } from 'shared/util/Time';
+import { Moment } from 'moment';
 
 interface ResultsProps {
   results: UserExpense[];
@@ -25,22 +30,42 @@ class ResultsViewImpl extends React.Component<ResultsProps> {
     );
   }
 
+  get hasResultsFromPreviousYears(): boolean {
+    const today = toMoment();
+    return (
+      this.props.results.find(r => !today.isSame(r.date, 'year')) !== undefined
+    );
+  }
+
   private renderResults() {
     if (!this.props.results || this.props.results.length < 1) {
       return <Info>Ei tuloksia, tarkista hakuehdot</Info>;
     }
+    const showYears = this.hasResultsFromPreviousYears;
+    let lastResDate: Moment | undefined;
     return (
       <ExpenseTableLayout className="padding">
         <tbody>
-          {this.props.results.map(e => (
-            <ExpenseRow
-              key={e.id}
-              expense={e}
-              onUpdated={this.props.onUpdate}
-              addFilter={noop}
-              userData={this.props.userData}
-            />
-          ))}
+          {this.props.results.map(e => {
+            const mom = toMoment(e.date);
+            const header =
+              showYears &&
+              (!lastResDate || !lastResDate.isSame(mom, 'year')) ? (
+                <YearHeader>Vuosi {mom.year()}</YearHeader>
+              ) : null;
+            lastResDate = mom;
+            return (
+              <React.Fragment key={e.id}>
+                {header}
+                <ExpenseRow
+                  expense={e}
+                  onUpdated={this.props.onUpdate}
+                  addFilter={noop}
+                  userData={this.props.userData}
+                />
+              </React.Fragment>
+            );
+          })}
         </tbody>
       </ExpenseTableLayout>
     );
@@ -58,6 +83,14 @@ const ResultsArea = styled.div`
 const Header = styled.div`
   color: ${secondaryColors.dark};
   margin: 8px 24px;
+`;
+
+const YearHeader = styled(AllColumns)`
+  color: ${secondaryColors.dark};
+  padding: 0px 8px;
+  background-color: ${gray.light};
+  width: 100%;
+  font-size: 16px;
 `;
 
 const Info = styled.div`
