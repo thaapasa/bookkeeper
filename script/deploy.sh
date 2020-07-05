c55f355a93e020193216aa16a2ae1ce68f72a6d8
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2020-07-05 15:20:07 +0300

    Update deploy script to target full hostname

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,16 +3,18 @@
 pushd . >/dev/null
 cd `dirname $0`/..
 
+HOST=kukkaro.pomeranssi.fi
+
 export REV=`git rev-parse HEAD | cut -c 1-8`
 script/build-prod.sh
 
 echo "Copying files to production (rev $REV)..."
 
-ssh deployer@pomeranssi.fi "mkdir -p bookkeeper/deploy" || exit -1
-scp deploy/client-$REV.tar.gz deploy/server-$REV.tar.gz deployer@pomeranssi.fi:~/bookkeeper/deploy || exit -1
+ssh deployer@$HOST "mkdir -p bookkeeper/deploy" || exit -1
+scp deploy/client-$REV.tar.gz deploy/server-$REV.tar.gz deployer@$HOST:~/bookkeeper/deploy || exit -1
 
 echo "Deploying on server..."
 
-ssh deployer@pomeranssi.fi "bash --login -c 'cd ~/bookkeeper && git pull && script/install-prod.sh $REV'"
+ssh deployer@$HOST "bash --login -c 'cd ~/bookkeeper && git pull && script/install-prod.sh $REV'"
 
 popd >/dev/null
