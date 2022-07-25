1e1c67340ca6a577e14857e52a8de0cbe69acebb
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2022-07-25 20:07:03 +0300

    Add some layout to split UI

--- src/client/ui/expense/split/ExpenseSplitDialog.tsx
+++ src/client/ui/expense/split/ExpenseSplitDialog.tsx
@@ -1,8 +1,10 @@
-import { Dialog, Typography } from '@mui/material';
+import { Cancel } from '@mui/icons-material';
+import { Button, Dialog, Grid, Typography } from '@mui/material';
 import * as React from 'react';
 
 import { UserExpense } from 'shared/types/Expense';
 import { ExpenseSplit } from 'shared/types/ExpenseSplit';
+import { Add, Split } from 'client/ui/Icons';
 import { isMobileSize } from 'client/ui/Styles';
 
 import { ExpenseDialogProps } from '../dialog/ExpenseDialog';
@@ -14,6 +16,7 @@ export const ExpenseSplitDialog: React.FC<ExpenseDialogProps> = ({
   original,
   onClose,
   windowSize,
+  categoryMap,
 }) => {
   const isMobile = isMobileSize(windowSize);
 
@@ -35,13 +38,44 @@ export const ExpenseSplitDialog: React.FC<ExpenseDialogProps> = ({
       fullScreen={isMobile}
     >
       <SplitHeader expense={original} />
-      <ExpenseDialogContent dividers={true} onClick={() => onClose(null)}>
+      <ExpenseDialogContent dividers={true}>
         <Typography color="text.secondary" variant="body2">
           Pilko kirjaus osiin
         </Typography>
-        {splits.map((s, i) => (
-          <SplitRow key={i} split={s} editSum={i !== 0} />
-        ))}
+        <Grid container>
+          {splits.map((s, i) => (
+            <SplitRow
+              key={i}
+              split={s}
+              editSum={i !== 0}
+              categoryMap={categoryMap}
+            />
+          ))}
+          <Grid item xs={4} container justifyContent="flex-start">
+            <Button startIcon={<Add />} variant="contained" color="secondary">
+              Lisää rivi
+            </Button>
+          </Grid>
+          <Grid item xs={4} container justifyContent="center">
+            <Button
+              startIcon={<Cancel />}
+              variant="outlined"
+              onClick={() => onClose(null)}
+            >
+              Peruuta
+            </Button>
+          </Grid>
+          <Grid item xs={4} container justifyContent="flex-end">
+            <Button
+              startIcon={<Split />}
+              variant="contained"
+              color="primary"
+              disabled
+            >
+              Pilko
+            </Button>
+          </Grid>
+        </Grid>
       </ExpenseDialogContent>
     </Dialog>
   );
--- src/client/ui/expense/split/SplitRow.tsx
+++ src/client/ui/expense/split/SplitRow.tsx
@@ -1,12 +1,48 @@
+import { Grid } from '@mui/material';
 import * as React from 'react';
 
 import { ExpenseSplit } from 'shared/types/ExpenseSplit';
+import Money from 'shared/util/Money';
+import { getFullCategoryName } from 'client/data/Categories';
+import { TextEdit } from 'client/ui/component/TextEdit';
+import { Delete } from 'client/ui/Icons';
 
-interface SplitRowProps {
+import { ToolIconButton } from '../details/ExpenseInfoTools';
+import { ExpenseDialogProps } from '../dialog/ExpenseDialog';
+
+interface SplitRowProps extends Pick<ExpenseDialogProps, 'categoryMap'> {
   split?: ExpenseSplit;
   editSum: boolean;
 }
 
-export const SplitRow: React.FC<SplitRowProps> = ({ split }) => {
-  return <>Kirjaus {split?.sum}</>;
+export const SplitRow: React.FC<SplitRowProps> = props => {
+  const { split, categoryMap } = props;
+  const [edit] = React.useState(false);
+  const showEditor = edit || !split;
+  return showEditor ? (
+    <SplitEditor {...props} />
+  ) : (
+    <>
+      <Grid xs={4}>{split.title}</Grid>{' '}
+      <Grid xs={5}>{getFullCategoryName(split.categoryId, categoryMap)} </Grid>
+      <Grid xs={2}>{Money.from(split.sum).format()}</Grid>
+      <Grid xs={1}>
+        <ToolIconButton>
+          <Delete />
+        </ToolIconButton>
+      </Grid>
+    </>
+  );
+};
+
+const SplitEditor: React.FC<SplitRowProps> = ({ split }) => {
+  const [title, setTitle] = React.useState(split?.title ?? '');
+  return (
+    <>
+      <Grid xs={4}>
+        <TextEdit label="Nimi" value={title} onChange={setTitle} />
+      </Grid>
+      <Grid xs={8}>Yeah</Grid>
+    </>
+  );
 };
