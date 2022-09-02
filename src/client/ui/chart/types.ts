29cbac7359d96260d2890aa1bf9e427ad8674d3d
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-02 15:46:57 +0300

    Rewrite statistics graph MVP with Recharts

--- src/client/ui/chart/types.ts
+++ src/client/ui/chart/types.ts
@@ -1,31 +1,4 @@
-import * as d3Axis from 'd3-axis';
-
 export type Domain = string | number;
-
-export interface ChartScales<X extends Domain> {
-  xScale: d3Axis.AxisScale<X>;
-  yScale: d3Axis.AxisScale<number>;
-}
-
-export interface ChartMargins {
-  bottom: number;
-  top: number;
-  left: number;
-  right: number;
-}
-
-export type ChartOrient = 'Bottom' | 'Left' | 'Top' | 'Right';
-
-export interface CommonChartProps<X extends Domain> {
-  scales: ChartScales<X>;
-  margins: ChartMargins;
-  svgDimensions: { height: number; width: number };
-  maxValue: number;
-}
-
-export type CommonChartDomain<P> = P extends CommonChartProps<infer D>
-  ? D
-  : never;
 
 export type ChartDataLine<X extends Domain> = {
   key: string;
--- src/client/ui/statistics/StatisticsChart.tsx
+++ src/client/ui/statistics/StatisticsChart.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
 
-import { CategoryStatistics } from 'shared/types/Statistics';
-import { numberRange } from 'shared/util/Arrays';
+import {
+  CategoryStatistics,
+  CategoryStatisticsData,
+} from 'shared/types/Statistics';
+import { groupBy, numberRange } from 'shared/util/Arrays';
 import Money from 'shared/util/Money';
+import { recordFromPairs, typedKeys } from 'shared/util/Objects';
+import { toMoment } from 'shared/util/Time';
+import { dateRangeToMomentRange, MomentRange } from 'shared/util/TimeRange';
 
-import { createLabeledChart } from '../chart/LabeledChart';
+import { getChartColor } from '../chart/ChartColors';
+import { ChartDataLine } from '../chart/types';
 import { MeasureSize } from '../utils/MeasureSize';
 import { Size } from '../utils/useElementSize';
-import { StatisticsBarGraph, StatisticsLineGraph } from './StatisticsGraph';
 import { Months } from './types';
-
-const LabeledLineChart = createLabeledChart(StatisticsLineGraph, 'point');
-const LabeledBarChart = createLabeledChart(StatisticsBarGraph, 'band');
-
-const useLines = false;
 
 /**
  * Statistics chart data:
@@ -22,26 +24,109 @@ const useLines = false;
  * YDomain = number (euros)
  */
 
-const months = numberRange(1, 12);
-
-const StatisticsChartImpl: React.FC<{
+const StatisticsBarGraph: React.FC<{
   statistics: CategoryStatistics;
   size: Size;
 }> = ({ statistics, size }) => {
-  const maxValue = Object.values(statistics.statistics)
-    .flat()
-    .reduce((p, c) => (p.gt(c.sum) ? p : new Money(c.sum)), new Money(0));
-
-  const Chart = useLines ? LabeledLineChart : LabeledBarChart;
+  const chartData = convertData(statistics);
+  const chartValues = Object.values(chartData);
+  const converted = numberRange(0, 11).map(m => {
+    const monthData: Record<string, any> = { month: Months[m] };
+    chartValues.forEach(e => (monthData[e.key] = e[m]));
+    return monthData;
+  });
   return (
-    <Chart
-      size={size}
-      maxValue={maxValue.valueOf()}
-      domain={months}
-      data={statistics}
-      labelFormatter={m => Months[m - 1]}
-    />
+    <BarChart
+      width={size.width}
+      height={300}
+      data={converted}
+      margin={ChartMargins}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Legend />
+      {chartValues.map(v => (
+        <Bar key={v.key} dataKey={v.key} fill={v.color} name={v.key} />
+      ))}
+    </BarChart>
   );
 };
 
-export const StatisticsChart = MeasureSize(StatisticsChartImpl);
+export const StatisticsChart = MeasureSize(StatisticsBarGraph);
+
+const ChartMargins = { left: 0, top: 32, right: 32, bottom: 0 };
+
+function convertData(data: CategoryStatistics): ChartDataLine<number>[] {
+  const keys = typedKeys(data.statistics);
+  const range = dateRangeToMomentRange(data.range);
+  return keys
+    .map((k, i) => convertAllYears(k, i, data.statistics[k], range))
+    .flat();
+}
+
+function convertAllYears(
+  key: string,
+  categoryIdx: number,
+  data: CategoryStatisticsData[],
+  range: MomentRange
+): ChartDataLine<string>[] {
+  const thisYear = toMoment().year();
+  const byYears = groupBy(i => i.month.substring(0, 4), data);
+  return typedKeys(byYears).map(year =>
+    convertOneYear(
+      `${key}-${year}`,
+      categoryIdx,
+      thisYear - Number(year),
+      byYears[year],
+      range
+    )
+  );
+}
+
+function convertOneYear(
+  key: string,
+  categoryIdx: number,
+  yearIdx: number,
+  data: CategoryStatisticsData[],
+  range: MomentRange
+): ChartDataLine<string> {
+  const line = yearDataToDataPoints(data, range);
+  return {
+    ...recordFromPairs(line.map(([d, v]) => [String(d), v])),
+    key,
+    color: getChartColor(categoryIdx, yearIdx),
+  } as any;
+}
+
+function yearDataToDataPoints(
+  data: CategoryStatisticsData[],
+  range: MomentRange
+): [number, number][] {
+  if (data.length < 1) {
+    return [];
+  }
+  const inputMonths = data.map<[number, number]>(d => [
+    Number(d.month.substring(5, 8)) - 1,
+    Money.from(d.sum).valueOf(),
+  ]);
+  const inputMap = recordFromPairs(inputMonths);
+
+  const minMonth = data[0].month;
+  const startPoint =
+    !minMonth || range.startTime.isBefore(`${minMonth}-01`)
+      ? 0
+      : inputMonths[0][0];
+
+  const maxMonth = data[data.length - 1]?.month;
+  const maxMonthM = maxMonth ? toMoment(`${maxMonth}-01`) : undefined;
+  const endPoint = !maxMonthM
+    ? 11
+    : range.endTime.isAfter(maxMonthM)
+    ? range.endTime.isAfter(maxMonthM.endOf('year'))
+      ? 11
+      : range.endTime.month()
+    : inputMonths[inputMonths.length - 1][0];
+
+  return numberRange(startPoint, endPoint).map(m => [m, inputMap[m] ?? 0]);
+}
