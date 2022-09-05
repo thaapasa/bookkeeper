3bfa2c4d417a3f5871ff939134a6811f45338a5a
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2022-09-05 21:26:17 +0300

    Extract /source API

--- src/server/api/Api.ts
+++ src/server/api/Api.ts
@@ -2,9 +2,8 @@ import debug from 'debug';
 import { Router } from 'express';
 
 import { ApiStatus } from 'shared/types/Api';
-import { Source, User } from 'shared/types/Session';
+import { User } from 'shared/types/Session';
 import { toMoment } from 'shared/util/Time';
-import { SourceDb } from 'server/data/SourceDb';
 import { UserDb } from 'server/data/UserDb';
 import { createErrorHandler } from 'server/server/ErrorHandler';
 import { Requests } from 'server/server/RequestHandling';
@@ -14,8 +13,8 @@ import { AdminDb, DbStatus } from '../data/admin/Admin';
 import { createCategoryApi } from './CategoryApi';
 import { createExpenseApi } from './ExpenseApi';
 import { createSessionApi } from './SessionApi';
+import { createSourceApi } from './SourceApi';
 import { createStatisticsApi } from './StatisticsApi';
-import { IdParamType } from './Validations';
 
 const log = debug('bookkeeper:api');
 
@@ -29,6 +28,7 @@ export function createApi() {
   api.use('/category', createCategoryApi());
   api.use('/expense', createExpenseApi());
   api.use('/statistics', createStatisticsApi());
+  api.use('/source', createSourceApi());
 
   // GET /api/status
   api.get(
@@ -60,26 +60,6 @@ export function createApi() {
     Requests.txRequest(
       (tx, session, req): Promise<User> =>
         UserDb.getById(tx, session.group.id, parseInt(req.params.id, 10)),
-      true
-    )
-  );
-
-  // GET /api/source/list
-  api.get(
-    '/source/list',
-    Requests.txRequest(
-      (tx, session): Promise<Source[]> => SourceDb.getAll(tx, session.group.id),
-      true
-    )
-  );
-
-  // GET /api/source/:id
-  api.get(
-    '/source/:id',
-    Requests.validatedTxRequest(
-      { params: IdParamType },
-      (tx, session, { params }): Promise<Source> =>
-        SourceDb.getById(tx, session.group.id, params.id),
       true
     )
   );
---
+++ src/server/api/SourceApi.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+
+import { Source } from 'shared/types/Session';
+import { SourceDb } from 'server/data/SourceDb';
+import { Requests } from 'server/server/RequestHandling';
+
+import { IdParamType } from './Validations';
+
+/**
+ * Creates source API router.
+ * Assumed attach path: `/api/source`
+ */
+export function createSourceApi() {
+  const api = Router();
+
+  // GET /api/source/list
+  api.get(
+    '/list',
+    Requests.txRequest(
+      (tx, session): Promise<Source[]> => SourceDb.getAll(tx, session.group.id),
+      true
+    )
+  );
+
+  // GET /api/source/:id
+  api.get(
+    '/:id',
+    Requests.validatedTxRequest(
+      { params: IdParamType },
+      (tx, session, { params }): Promise<Source> =>
+        SourceDb.getById(tx, session.group.id, params.id),
+      true
+    )
+  );
+
+  return api;
+}
