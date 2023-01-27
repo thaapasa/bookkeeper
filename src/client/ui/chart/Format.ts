79b90510c7237187f6cd3758c881cc026c2eb2a7
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2023-01-27 23:01:24 +0200

    Update some more deps

--- src/client/ui/chart/Format.ts
+++ src/client/ui/chart/Format.ts
@@ -5,7 +5,7 @@ import { Size } from '../Types';
 export const formatMoneyThin = (v: number) =>
   v > 1000 ? `${Math.round(v / 1000)}K` : `${v}`;
 
-export const formatMoney = (v: number) => {
+export const formatMoney = (v: number | string) => {
   try {
     return Money.from(v).format(0);
   } catch (e) {
