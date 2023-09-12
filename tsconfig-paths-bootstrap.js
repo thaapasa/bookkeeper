0daf6199decf655582345509ae5b89ba91b2764f
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-09-12 14:41:00 +0300

    Remove bootrap

--- tsconfig-paths-bootstrap.js
+++
@@ -1,8 +0,0 @@
-const tsConfig = require('./tsconfig.prod.json');
-const tsConfigPaths = require('tsconfig-paths');
-
-const baseUrl = './src'; // Either absolute or relative path. If relative it's resolved to current working directory.
-tsConfigPaths.register({
-  baseUrl,
-  paths: tsConfig.compilerOptions.paths,
-});
