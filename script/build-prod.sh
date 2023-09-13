8fb403bd1ca6e8ab5e591ea8dd53343ca83ccf8f
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-09-13 15:59:29 +0300

    Update prod build script

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
+SERVER_SRC="src/server src/shared src/typings"
+
 pushd . >/dev/null
 cd `dirname $0`/..
 
 echo "Updating dependencies"
-yarn || exit -1
+bun install || exit -1
 
 mkdir -p deploy || exit -1
-yarn update-revisions
+bun update-revisions
 
 REV=`git rev-parse HEAD | cut -c 1-8`
-echo "Building server, revision $REV..."
+echo "Packaging server files, revision $REV..."
 
-yarn clean || exit -1
-yarn build-server || exit -1
+bun clean || exit -1
 
-cd build-server
-tar czvf ../deploy/server-$REV.tar.gz . || exit -1
-cd ..
+tar czvf deploy/server-$REV.tar.gz $SERVER_SRC || exit -1
 
-echo "Server built"
+echo "Server packaged"
 
 echo "Building client, revision $REV"
 
 yarn clean || exit -1
 yarn build-client || exit -1
 
-cd build
+cd dist
 tar czvf ../deploy/client-$REV.tar.gz . || exit -1
 cd ..
 
 echo "Client built"
 
-echo "Deployed revision $REV"
+echo "Built revision $REV"
 
 popd >/dev/null
