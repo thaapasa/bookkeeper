b2f718784164623b6a316a74460f0f13d3b25227
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-09-14 09:13:13 +0300

    Add script to build server

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SERVER_SRC="src/server src/shared src/typings"
-
 pushd . >/dev/null
 cd `dirname $0`/..
 
@@ -11,12 +9,18 @@ bun install || exit -1
 mkdir -p deploy || exit -1
 bun update-revisions
 
-REV=`git rev-parse HEAD | cut -c 1-8`
-echo "Packaging server files, revision $REV..."
-
 bun clean || exit -1
 
-tar czvf deploy/server-$REV.tar.gz $SERVER_SRC || exit -1
+REV=`git rev-parse HEAD | cut -c 1-8`
+
+echo "Building server, revision $REV..."
+bun run build-server
+
+echo "Packaging server files, revision $REV..."
+
+cd build-server
+tar czvf ../deploy/server-$REV.tar.gz . || exit -1
+cd ..
 
 echo "Server packaged"
 
---
+++ src/build-server.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+
+async function bundle() {
+  console.log('Bunding code');
+  const build = await Bun.build({
+    entrypoints: ['src/server/BookkeeperServer.ts'],
+    outdir: './build-server',
+    target: 'bun',
+    sourcemap: 'external',
+  });
+
+  for (const output of build.outputs) {
+    console.log('Built file', output.path);
+  }
+}
+
+bundle().catch(e => {
+  console.log('Error building app', e);
+});
