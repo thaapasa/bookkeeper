bba8ad64bf82d226a0f2e476c3fee24cf6759829
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-11-04 15:36:06 +0200

    Fix MeasureSize and debounce to not render component all the time

--- src/client/ui/utils/MeasureSize.tsx
+++ src/client/ui/utils/MeasureSize.tsx
@@ -5,7 +5,10 @@ import { useDebounced } from '../hooks/useDebounced';
 import { useElementSize } from '../hooks/useElementSize';
 import { Size } from '../Types';
 
-export function MeasureSize<T>(component: React.ComponentType<T & { size: Size }>) {
+export function MeasureSize<T>(
+  component: React.ComponentType<T & { size: Size }>,
+  height?: number | string,
+) {
   const MeasuredComponent: React.FC<Omit<T, 'size'> & { className?: string }> = ({
     className,
     ...props
@@ -13,19 +16,24 @@ export function MeasureSize<T>(component: React.ComponentType<T & { size: Size }
     const containerRef = React.useRef<HTMLDivElement>(null);
 
     const size = useElementSize(containerRef);
-
-    const debouncedSize = useDebounced(size);
+    const debounced = useDebounced(size) ?? size;
 
     const Component = component as any;
-    return (
-      <MeasureDiv ref={containerRef} className={className}>
-        {size ? <Component size={debouncedSize ?? size} {...props} /> : null}
-      </MeasureDiv>
+    return React.useMemo(
+      () => (
+        <MeasureDiv ref={containerRef} className={className} height={height}>
+          {size ? <Component size={debounced} {...props} /> : null}
+        </MeasureDiv>
+      ),
+      // eslint-disable-next-line
+      [Component, className, debounced, ...(Object.keys(props)), ...(Object.values(props))],
     );
   };
   return MeasuredComponent;
 }
 
 const MeasureDiv = styled('div')`
-  width: 100%;
+  display: flex;
+  flex: 1;
+  ${(props: { height?: string | number }) => (props.height ? `height: ${props.height}` : '')}
 `;
