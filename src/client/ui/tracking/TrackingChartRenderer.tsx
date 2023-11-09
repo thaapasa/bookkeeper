c1b939ec8b83d2f532a8cd91ffaff2a51567e464
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-11-09 20:07:15 +0200

    Render combined chart

--- src/client/ui/tracking/TrackingChartRenderer.tsx
+++ src/client/ui/tracking/TrackingChartRenderer.tsx
@@ -1,8 +1,19 @@
 import styled from '@emotion/styled';
 import * as React from 'react';
-import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis } from 'recharts';
+import {
+  Area,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ComposedChart,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+} from 'recharts';
 
-import { TrackingData, TrackingStatistics } from 'shared/types';
+import { TrackingChartType, TrackingData, TrackingStatistics } from 'shared/types';
+import { partition } from 'shared/util';
 import { formatMoney } from 'client/ui/chart/Format';
 
 import { getChartColor } from '../chart/ChartColors';
@@ -16,18 +27,6 @@ interface TrackingChartProps {
   trackingData: TrackingData;
   size: Size;
 }
-
-export const TrackingChartRenderer: React.FC<TrackingChartProps> = props => {
-  return (
-    <ChartContainer>
-      {props.trackingData.chartType === 'bar' ? (
-        <TrackingBarChartRenderer {...props} />
-      ) : (
-        <TrackingLineChartRenderer {...props} />
-      )}
-    </ChartContainer>
-  );
-};
 
 export const TrackingLineChartRenderer: React.FC<TrackingChartProps> = ({
   data,
@@ -72,6 +71,56 @@ export const TrackingBarChartRenderer: React.FC<TrackingChartProps> = ({
         />
       ))}
     </BarChart>
+  );
+};
+
+export const TrackingCombinedRenderer: React.FC<TrackingChartProps> = ({
+  data,
+  size,
+  trackingData,
+}) => {
+  const [lines, bars] = partition(g => g.chartType === 'line', data.groups);
+  return (
+    <ComposedChart width={size.width} height={168} data={data.statistics} margin={Margins}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis hide dataKey="timeSlot" />
+      <Tooltip formatter={formatMoney} />
+      {lines.map((v, i) => (
+        <Area
+          type="monotone"
+          key={v.key}
+          dataKey={v.key}
+          fill={getChartColor(i + (trackingData.colorOffset ?? 0), 6)}
+          stroke={getChartColor(i + (trackingData.colorOffset ?? 0), 2)}
+          name={v.label ?? v.key}
+        />
+      ))}
+      {bars.map((v, i) => (
+        <Bar
+          type="monotone"
+          key={v.key}
+          dataKey={v.key}
+          fill={getChartColor(i + (trackingData.colorOffset ?? 0) + lines.length, 0)}
+          name={v.label ?? v.key}
+        />
+      ))}
+    </ComposedChart>
+  );
+};
+
+const ChartTypeRenderers: Record<TrackingChartType, React.FC<TrackingChartProps>> = {
+  bar: TrackingBarChartRenderer,
+  line: TrackingLineChartRenderer,
+  combined: TrackingCombinedRenderer,
+};
+
+export const TrackingChartRenderer: React.FC<TrackingChartProps> = props => {
+  const Renderer =
+    ChartTypeRenderers[props.trackingData.chartType ?? 'line'] ?? TrackingBarChartRenderer;
+  return (
+    <ChartContainer>
+      <Renderer {...props} />
+    </ChartContainer>
   );
 };
 
